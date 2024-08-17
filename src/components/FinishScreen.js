d4fdf9bc4001634fa57e15532fc34d923c67d600
Cameron <cameron.oldham@icloud.com>
2024-08-17 16:31:57 +0100

    Add all missing files and directories

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,26 @@
+import { useQuiz } from "../context/QuizContext";
+
+function FinishScreen() {
+  const { points, maxPossiblePoints, highscore, dispatch } = useQuiz();
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints}(
+        {Math.ceil(percentage)}%)
+      </p>
+
+      <p className="highscore">(Highscore: {highscore} points)</p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Reset
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
